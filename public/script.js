99b07202ffcb64d047dfddf81168d35265743a88
Mike <mikelautensack100@gmail.com>
2022-12-14 21:34:23 -0500

    started and almost finished the calculator

---
+++ public/script.js
@@ -0,0 +1,27 @@
+const clear = document.getElementById('clear');
+const plusMinus = document.getElementById('plus-minus');
+const percent = document.getElementById('percent');
+const divide = document.getElementById('divide');
+const multiply = document.getElementById('multiply');
+const subtraction = document.getElementById('subtraction');
+const addition = document.getElementById('addition');
+const decimal = document.getElementById('decimal');
+const equals = document.getElementById('equals');
+const numButton = document.getElementById('num-button');
+
+let otherButtons = [clear, plusMinus, percent, divide, multiply, subtraction, addition, decimal, equals];
+
+let opp1 = document.getElementById('opp1');
+let opp2 = document.getElementById('opp2');
+
+numButton.forEach(button => {
+    button.addEventListener('click', () => {
+
+    });
+});
+
+otherButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+    });
+});
