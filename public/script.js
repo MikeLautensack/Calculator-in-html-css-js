08a0c9351dca5cbf8f69b66feb3e3fb21cea6815
Mike <mikelautensack100@gmail.com>
2022-12-18 16:55:38 -0500

    finished calculator logic, may still add some finishing touchs to the styles but its mostly done

--- public/script.js
+++ public/script.js
@@ -2,61 +2,82 @@ const buttonPanel = document.querySelector('[data-button-panel]');
 const buttons = Array.from(buttonPanel.children);
 
 let displayPrimary = document.querySelector('[data-display-primary]');
-let displaySecondary = document.querySelector('[data-displays-secondary]');
+let displaySecondary = document.querySelector('[data-display-secondary]');
+
+let displayPrimaryValue = 0;
+let displaySecondaryValue = 0;
+let operator = '';
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
 
-        //number button & decimal code
-        if(displayPrimary.innerText == '') {
-            if(event.target.innerText == '.') {
-                displayPrimary.innerText = '0.'
+        
+
+        //number button & decimal button code
+        if(displayPrimary.textContent == '0') {
+            if(event.target.textContent == '.') {
+                displayPrimary.textContent = '0.'
             } else {
-                displayPrimary.innerText = event.target.innerText;
+                displayPrimary.textContent = event.target.textContent;
             }
         } else {
-            if(event.target.innerText == '.' && displayPrimary.innerText.includes('.')) return
-            displayPrimary.innerText = displayPrimary.innerText + event.target.innerText;
+            if(event.target.textContent == '.' && displayPrimary.textContent.includes('.')) return
+            displayPrimary.textContent = displayPrimary.textContent + event.target.textContent;
         }
 
         //operator button code
         if(event.target.matches('[data-add]') || event.target.parentNode.matches('[data-add]')) {
-            displaySecondary.innerText = displayPrimary.innerText;
-            displayPrimary.innerText = '';
+            displaySecondary.textContent = displayPrimary.textContent;
+            displayPrimary.textContent = '0';
+            operator = '+';
         } else if (event.target.matches('[data-subtract]') || event.target.parentNode.matches('[data-subtract]')) {
-            console.log('-');
+            displaySecondary.textContent = displayPrimary.textContent;
+            displayPrimary.textContent = '0';
+            operator = '-';
         } else if (event.target.matches('[data-multiply]') || event.target.parentNode.matches('[data-multiply]')) {
-            console.log('*');
+            displaySecondary.textContent = displayPrimary.textContent;
+            displayPrimary.textContent = '0';
+            operator = '*';
         } else if (event.target.matches('[data-divide]') || event.target.parentNode.matches('[data-divide]')) {
-            console.log('/');
+            displaySecondary.textContent = displayPrimary.textContent;
+            displayPrimary.textContent = '0';
+            operator = '/';
         }
     
         //equals button code
         if(event.target.matches('[data-equals]') || event.target.parentNode.matches('[data-equals]')) {
-            console.log('=');
+            displayPrimaryValue = Number(displayPrimary.textContent);
+            displaySecondaryValue = Number(displaySecondary.textContent);
+            displayPrimary.textContent = calculate(displaySecondaryValue, operator, displayPrimaryValue);
+            displaySecondary.textContent = '0';
+            console.log(displayPrimaryValue);
+            console.log(displaySecondaryValue);
+            console.log(operator);
         }
 
         //clear button code
         if(event.target.matches('[data-clear]') || event.target.parentNode.matches('[data-clear]')) {
-            displayPrimary.innerText = '';
-            displaySecondary.innerText = '';
-            
+            displayPrimary.textContent = '0';
+            displaySecondary.textContent = '0';
         }
 
         //plus-minus button code
         if(event.target.matches('[data-plus-minus]') || event.target.parentNode.matches('[data-plus-minus]')) {
-            console.log('+-');
+            displayPrimary.textContent = '-' + displayPrimary.textContent
         }
 
         //percent button code
         if(event.target.matches('[data-percent]') || event.target.parentNode.matches('[data-percent]')) {
-            console.log('%');
+            displayPrimary.textContent = Number(displayPrimary.textContent) / 100;
         }
 
     })
 })
 
-function calculate() {
-
-}
-
+function calculate (displaySecondaryValue, operator, displayPrimaryValue) {
+  
+    if (operator === '+') return displaySecondaryValue + displayPrimaryValue;
+    if (operator === '-') return displaySecondaryValue - displayPrimaryValue;
+    if (operator === '*') return displaySecondaryValue * displayPrimaryValue;
+    if (operator === '/') return displaySecondaryValue / displayPrimaryValue;
+  }
