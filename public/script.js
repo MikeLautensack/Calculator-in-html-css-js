fac9bde35a29b96186dd106933213556d7af3214
Mike <mikelautensack100@gmail.com>
2022-12-17 21:13:54 -0500

    spend 5 hrs figuring out how to make the svg elements trigger the same block of conditional logic as the parent button during an event

--- public/script.js
+++ public/script.js
@@ -7,8 +7,6 @@ let displaySecondary = document.querySelector('[data-displays-secondary]');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
 
-
-    
         //number button & decimal code
         if(displayPrimary.innerText == '') {
             if(event.target.innerText == '.') {
@@ -22,17 +20,38 @@ buttons.forEach(button => {
         }
 
         //operator button code
-        if(event.target.hasAttributes('data-add')) {
-            console.log('pass');
+        if(event.target.matches('[data-add]') || event.target.parentNode.matches('[data-add]')) {
+            displaySecondary.innerText = displayPrimary.innerText;
+            displayPrimary.innerText = '';
+        } else if (event.target.matches('[data-subtract]') || event.target.parentNode.matches('[data-subtract]')) {
+            console.log('-');
+        } else if (event.target.matches('[data-multiply]') || event.target.parentNode.matches('[data-multiply]')) {
+            console.log('*');
+        } else if (event.target.matches('[data-divide]') || event.target.parentNode.matches('[data-divide]')) {
+            console.log('/');
         }
     
         //equals button code
+        if(event.target.matches('[data-equals]') || event.target.parentNode.matches('[data-equals]')) {
+            console.log('=');
+        }
 
         //clear button code
+        if(event.target.matches('[data-clear]') || event.target.parentNode.matches('[data-clear]')) {
+            displayPrimary.innerText = '';
+            displaySecondary.innerText = '';
+            
+        }
 
         //plus-minus button code
+        if(event.target.matches('[data-plus-minus]') || event.target.parentNode.matches('[data-plus-minus]')) {
+            console.log('+-');
+        }
 
         //percent button code
+        if(event.target.matches('[data-percent]') || event.target.parentNode.matches('[data-percent]')) {
+            console.log('%');
+        }
 
     })
 })
